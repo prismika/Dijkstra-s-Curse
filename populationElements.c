f5244cd2ec83be30bfd6e812e14706b5dff9c616
Brantley Vose <brantmv@iastate.edu>
2019-02-27 00:02:03 -0600

    Stub init_entity_pc

--- populationElements.c
+++ populationElements.c
@@ -4,9 +4,18 @@
 void init_entity_npc(Entity *ent, Coordinate coord, char symbol, uint32_t characteristics){
 	ent->position = coord;
 	ent->symbol = symbol;
-
-	//ent->npc = &npc
+	ent->dead = false;
+	//end->pc = NULL;
+	//ent->npc = &npc;
 	switch(characteristics){
 		//Assign movement strategy to npc
 	}
+}
+
+void init_entity_pc(Entity *ent, Coordinate coord, char symbol){
+	ent->position = coord;
+	ent->symbol = symbol;
+	ent->dead = false;
+	//end->pc = &pc;
+	//ent->npc = NULL;
 }
--- populationElements.h
+++ populationElements.h
@@ -2,16 +2,17 @@
 #define POPULATIONELEMENTS_H
 #include "mapElements.h"
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef struct{
 	// PC *pc;
 	// NPC *npc;
 	Coordinate position;
 	char symbol;
+	bool dead;
 } Entity;
 
 void init_entity_npc(Entity *ent, Coordinate coord, char symbol, uint32_t characteristics);
-
-
+void init_entity_pc(Entity *ent, Coordinate coord, char symbol);
 
 #endif
