2bbd431b502dfc87ef0de340a7c77ae749a1555d
Brantley Vose <brantmv@iastate.edu>
2019-04-02 15:07:58 -0500

    Fill out cout<<blueprint operator (except for printing dice values).

--- monsterParser.cpp
+++ monsterParser.cpp
@@ -8,7 +8,7 @@ MonsterBlueprint * parser_load_monster_list(int * listSize){
 	*listSize = 1;
 	Dice d1,d2,d3;
 	MonsterBlueprint blueprint((char*)"Gregory",(char*)"He's a fast one",d1,d2,d3,
-		'x',MONSTER_RED,10);
+		'G',MONSTER_RED,10);
 	blueprintList[0] = blueprint;
 	return blueprintList;
 }
@@ -30,8 +30,50 @@ MonsterBlueprint::MonsterBlueprint(char * name, char * description,
 }
 
 std::ostream & operator<<(std::ostream &out, const MonsterBlueprint &r){
-	return out << "Name: "
-	<< r.name <<std::endl;
+	char * colorName;
+	switch(r.color){
+		case MONSTER_RED:
+		colorName = (char*) "Red";
+		break;
+		case MONSTER_GREEN:
+		colorName = (char*) "Green";
+		break;
+		case MONSTER_BLUE:
+		colorName = (char*) "Blue";
+		break;
+		case MONSTER_CYAN:
+		colorName = (char*) "Cyan";
+		break;
+		case MONSTER_YELLOW:
+		colorName = (char*) "Yellow";
+		break;
+		case MONSTER_MAGENTA:
+		colorName = (char*) "Magenta";
+		break;
+		case MONSTER_WHITE:
+		colorName = (char*) "White";
+		break;
+		case MONSTER_BLACK:
+		colorName = (char*) "Black";
+		break;
+	}
+	out << "Name: " << r.name << std::endl;
+	out << "Symbol: " << r.symbol << std::endl;
+	out << "Color: " << colorName << std::endl;
+	out << "Rarity: " << r.rarity << std::endl;
+	//out << "Speed: " << r.speed << std::endl;
+	//out << "Hitpoints: " <<r.hitpoints << std::endl;
+	//out << "Attack: " <<r.attackDamage << std::endl;
+	out << "Abilities: ";
+	int i;
+	for(i=0; i<r.abilitiesSize; i++){
+		out << r.abilities[i] << " ";
+	}
+	out<<std::endl;
+
+	out << r.description << std::endl;
+
+	return out;
 }
 
 Dice::Dice(int base, int count, int sides){
